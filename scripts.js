15e24c30b5e9b737f11fd66f8d9de940abaca31c
Ana Julia Gari  Albano <112116683+n4ju15@users.noreply.github.com>
2023-07-21 16:04:06 -0300

    Conversion functionality finished.

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em real
+    const currencyValueConverted = document.querySelector(".currency-value") //Outras moedas
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    if (currencySelect.value == "dolar") { //Se o select tiver selecionado o valor de dolar, entra aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect.value == "euro") { //Se o select tiver selecionado o valor de euro, entra aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { //Se o select tiver selecionado o valor de real, entra aqui
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+}
+
+convertButton.addEventListener("click", convertValues)
