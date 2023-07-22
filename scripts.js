1d9b0705f50cabc77c7dbe603a0a23de4bced9f1
n4ju15 <anajulia.garialbano15@gmail.com>
2023-07-22 20:36:12 -0300

    The Change Currency function changes the text above the flag according to the selected option.

--- scripts.js
+++ scripts.js
@@ -29,4 +29,17 @@ function convertValues() {
 
 }
 
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+    }
+}
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
