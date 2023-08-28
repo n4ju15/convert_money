517e0848a5cbe57d0169bee4827486554d7cb84c
Ana Julia Gari  Albano <112116683+n4ju15@users.noreply.github.com>
2023-08-28 13:55:46 -0300

    Added pound and bitcoin coins and revised project functionality.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
-const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
+const convertButton = document.querySelector(".convert-button") // Variável - Aqui está sendo mapeado o botão de converter
+const currencySelect = document.querySelector(".currency-select") // Variável - Aqui está sendo mapeado o select de converter para
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em real
-    const currencyValueConverted = document.querySelector(".currency-value") //Outras moedas
+    const inputCurrencyValue = document.querySelector(".input-currency").value //  Variável - Pega o valor do input => .value para trazer somente o valor dentro do input
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Variável - Mostra o valor inserido no input (Real R$) => Valor para converter
+    const currencyValueConverted = document.querySelector(".currency-value") // Variável - Mostra o valor inserido no input de outras moedas (Dólar e Euro) => Valor convertido
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    const dolarToday = 4.89 // Variável - Com valor fixo do dólar
+    const euroToday = 5.29 //  Variável - Com valor fixo do euro
+    const libraToday = 6.17 // Variável - Com valor fixo da libra
+    const bitcoinToday = 128.024 // Variável - Com valor fixo da bitcoin
 
     if (currencySelect.value == "dolar") { //Se o select tiver selecionado o valor de dolar, entra aqui
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -21,17 +23,31 @@ function convertValues() {
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
     }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+    if (currencySelect.value == "btc") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC",
+            minimumFractionDigits: 8, // Número de casas decimais após o ponto
+            maximumFractionDigits: 8, 
+        }).format(inputCurrencyValue / bitcoinToday)
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { //Se o select tiver selecionado o valor de real, entra aqui
-        style: "currency",
+        style: "currency", // Configurações do Intl.NumberFormat
         currency: "BRL"
-    }).format(inputCurrencyValue)
+    }).format(inputCurrencyValue) // Formata o item que estiver entre parênteses
 
 }
 
 function changeCurrency() {
-    const currencyName = document.getElementById("currency-name")
-    const currencyImage = document.querySelector(".currency-image")
+    const currencyName = document.getElementById("currency-name") //  Variável que busca o nome da moeda selecionada
+    const currencyImage = document.querySelector(".currency-image") // Variável que busca a imagem da moeda selecionada
 
     if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar americano" // Troca o texto na parte de cima das bandeiras de acordo com o país selecionado
@@ -43,7 +59,18 @@ function changeCurrency() {
         currencyImage.src = "./assets/euro.png"
     }
 
-    convertValues() // Exibe o valor convertido para a moeda selecionado caso o usuário troque a moeda depois de inserir o valor
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+    }
+
+    if (currencySelect.value == "btc") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "./assets/bitcoin.png"
+    }
+
+    convertValues() // Primeiro o JS executa essa função e troca as imagens e os nomes das moedas, ai chamamos a função convertValues para que ela faça a parte da conversão
+    // Exibe o valor convertido para a moeda selecionado caso o usuário troque a moeda depois de inserir o valor
 }
 
 currencySelect.addEventListener("change", changeCurrency) // Ao evento de click no campo de converter para, ele troca a bandeira, os textos e as moedas
